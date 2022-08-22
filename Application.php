d4c4edd62d2f229717ee60cebbd4af789e5bba55
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-22 18:09:26 +0300

    Initial commit

---
+++ Application.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\core;
+
+use app\core\db\Database;
+
+class Application
+{
+    public static string $ROOT_DIR;
+
+    public string $layout = 'main';
+    public string $userClass;
+    public Router $router;
+    public Request $request;
+    public Response $response;
+    public Session $session;
+    public Database $db;
+    public View $view;
+
+    public static Application $app;
+    public ?Controller $controller = null;
+    public ?UserModel $user;
+
+    public function __construct($rootPath, array $config)
+    {
+        $this->userClass = $config['userClass'];
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
+        $this->request = new Request();
+        $this->db = new Database($config['db']);
+        $this->response = new Response();
+        $this->session = new Session();
+        $this->router = new Router($this->request, $this->response);
+        $this->user = null;
+        $this->view = new View();
+
+        $primaryValue = $this->session->get('user');
+        if ($primaryValue){
+            $primaryKey = $this->userClass::primaryKey();
+            $this->user = $this->userClass::findOne([$primaryKey => $primaryValue]);
+        } else {
+            $this->user = null;
+        }
+    }
+
+    public function run()
+    {
+        try {
+            echo $this->router->resolve();
+        } catch (\Exception $e) {
+            $this->response->setStatusCode($e->getCode());
+            echo $this->view->renderView('_error', [
+                'exception' => $e
+            ]);
+        }
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+    
+    public function setController(\app\core\Controller $controller): void
+    {
+        $this->controller = $controller;
+    }
+
+    public function login(UserModel $user)
+    {
+        $this->user = $user;
+        $className = get_class($user);
+        $primaryKey = $className::primaryKey();
+        $primaryValue = $user->{$primaryKey};
+        Application::$app->session->set('user', $primaryValue);
+        return true;
+    }
+
+    public function logout()
+    {
+        $this->user = null;
+        $this->session->remove('user');
+    }
+
+    public static function isGuest()
+    {
+        return !self::$app->user;
+    }
+}
